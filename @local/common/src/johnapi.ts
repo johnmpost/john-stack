50e0dfebd439c0c0c15d68541bcd1b14ddbbee86
john <johnmichaelpost@gmail.com>
2024-06-06 00:47:10 -0500

    major breakthrough

--- @local/common/src/johnapi.ts
+++ @local/common/src/johnapi.ts
@@ -1,26 +1,16 @@
-import { flow, pipe, Schema, Ef, O, A, E } from "./toolbox";
+import { pipe, Schema, Ef, E } from "./toolbox";
 import { NetworkError, networkError } from "./errors";
-import { LiteralValue } from "@effect/schema/AST";
-import type * as Types from "effect/Types";
-import { Struct, tag, TypeLiteral } from "@effect/schema/Schema";
-import { AST } from "@effect/schema";
-
-type TaggedStructFields = {
-  readonly [x: string]: Schema.Schema.AnyNoContext;
-};
-
-type TaggedStruct<
-  Tag extends AST.LiteralValue,
-  Fields extends TaggedStructFields
-> = Struct<{ _tag: tag<Tag> } & Fields>;
 
 export type WebFunctionDef<
-  Name extends LiteralValue,
-  Params extends TaggedStructFields,
+  Name extends string,
+  Params,
   Result,
   EncodedResult
 > = {
-  params: Schema.TaggedStruct<Name, Params>;
+  params: Schema.Struct<{
+    _tag: Schema.Literal<[Name]>;
+    params: Schema.Schema<Params>;
+  }>;
   result: Schema.Schema<Result, EncodedResult>;
 };
 
@@ -33,23 +23,13 @@ export type WebFunctionImpl<Def> = Def extends WebFunctionDef<
   ? (params: Params) => Result
   : never;
 
-export type WebFunction<
-  Name extends LiteralValue,
-  Params extends TaggedStructFields,
-  Result,
-  EncodedResult
-> = {
-  spec: WebFunctionDef<Name, Params, Result, EncodedResult>;
+export type WebFunction<Name extends string, Params, Result, EncodedResult> = {
+  def: WebFunctionDef<Name, Params, Result, EncodedResult>;
   impl: (params: Params) => Result;
 };
 
-export type Invoker = <
-  Name extends LiteralValue,
-  Params extends TaggedStructFields,
-  Result,
-  EncodedResult
->(
-  webFunction: WebFunctionDef<Name, Params, Result, EncodedResult>
+export type Invoker = <Name extends string, Params, Result, EncodedResult>(
+  webFunctionDef: WebFunctionDef<Name, Params, Result, EncodedResult>
 ) => (params: Params) => Ef.Effect<Result, NetworkError>;
 
 const postJson = (url: string) => (jsonBody: string) =>
@@ -65,168 +45,51 @@ const postJson = (url: string) => (jsonBody: string) =>
     })
   );
 
-type Constraint<T> = "jingle" extends keyof T ? never : T;
-const func = <T>(param: Constraint<T>) => param.jingle;
-func({ jingle: "pingle" });
-
-type NoContext<T> = Schema.Schema.Context<T> extends never ? T : never;
-type NoContextParams<T extends { params: any }> =
-  T["params"] extends Schema.Schema.AnyNoContext ? T : never;
-
-type EnforceAllProperties<T, U> = {
-  [K in keyof T]: T[K] extends U ? T[K] : never;
-};
-
-// const mkTaggedStruct = <
-//   Name extends LiteralValue,
-//   Fields extends EnforceAllProperties<
-// >(
-//   name: Name,
-//   fields: NoContext<Fields>,
-//   toMake: Types.Simplify<
-//     Struct.Constructor<{ _tag: tag<Name> } & NoContext<Fields>>
-//   >
-// ) => {
-//   const mySt = Schema.TaggedStruct(name, fields);
-//   mySt.make(toMake);
-//   Schema.encodeSync(mySt); // Argument of type 'TaggedStruct<Name, Fields>' is not assignable to parameter of type 'Schema<Simplify<Type<{ _tag: tag<Name>; } & Fields>>, Simplify<Encoded<{ _tag: tag<Name>; } & Fields, []>>, never>'
-//   return mySt;
-// };
-
-type Token = Schema.PropertySignature.Token;
-
-type FieldsNoContext = {
-  readonly [x: PropertyKey]:
-    | Schema.Schema.AnyNoContext
-    | Schema.PropertySignature<
-        Token,
-        any,
-        PropertyKey,
-        Token,
-        any,
-        boolean,
-        never
-      >
-    | Schema.PropertySignature<
-        Token,
-        never,
-        PropertyKey,
-        Token,
-        any,
-        boolean,
-        never
-      >
-    | Schema.PropertySignature<
-        Token,
-        any,
-        PropertyKey,
-        Token,
-        never,
-        boolean,
-        never
-      >
-    | Schema.PropertySignature<
-        Token,
-        never,
-        PropertyKey,
-        Token,
-        never,
-        boolean,
-        never
-      >;
-};
-const mkTaggedStruct = <
-  Name extends AST.LiteralValue,
-  Fields extends FieldsNoContext
->(
-  name: Name,
-  fields: Fields
-) => {
-  const schema = Schema.TaggedStruct(name, fields);
-  const schema_: Schema.Schema<typeof schema.Type, typeof schema.Encoded> =
-    Schema.TaggedStruct(name, fields) as any;
-  Schema.encodeSync(schema_);
-  return schema_;
-};
-
-const MyTs = mkTaggedStruct("SignUpUser", {
-  email: Schema.String,
-  password: Schema.String,
-});
-
-Schema.encodeSync(MyTs)({ email: "", password: "", _tag: MyTs.Encoded._tag });
-
-type myts = typeof MyTs.Type;
-
-const SignUpUser = Schema.TaggedStruct("SignUpUser", {
-  email: Schema.String,
-  password: Schema.String,
-});
-type test = Struct.Context<typeof SignUpUser.fields>;
-
-const test2 = make(SignUpUser)({ email: "john", password: "234" });
-const test3 = Schema.encodeSync(Schema.parseJson(SignUpUser))(test2);
-
 export const mkInvoke =
   (url: string) =>
-  <
-    Name extends LiteralValue,
-    Params extends TaggedStructFields,
-    Result,
-    EncodedResult
-  >(
-    webFunction: NoContextParams<
-      WebFunctionDef<Name, Params, Result, EncodedResult>
-    >
+  <Name extends string, Params, Result, EncodedResult>(
+    webFunctionDef: WebFunctionDef<Name, Params, Result, EncodedResult>
   ) =>
-  // (params: Types.Simplify<Struct.Constructor<TaggedStructFields>>) =>
-  (params: Types.Simplify<Struct.Constructor<{ _tag: tag<Name> } & Params>>) =>
-    // (params: Struct.Encoded<Params>) =>
+  (params: Params) =>
     pipe(
-      webFunction.params.make(params),
-      x => x,
-      // // webFunctionSpec.params.make(params),
-      Schema.encodeSync(webFunction.params)
-      // postJson(url),
-      // Ef.map(Schema.decodeSync(Schema.parseJson(webFunctionSpec.result)))
+      { _tag: webFunctionDef.params.Type._tag, params },
+      Schema.encodeSync(Schema.parseJson(webFunctionDef.params)),
+      postJson(url),
+      Ef.map(Schema.decodeSync(Schema.parseJson(webFunctionDef.result)))
     );
 
 export const mkWebFunction =
-  <Param, EncodedParam, Result, EncodedResult>(
-    spec: WebFunctionDef<Param, EncodedParam, Result, EncodedResult>
+  <Name extends string, Params, Result, EncodedResult>(
+    def: WebFunctionDef<Name, Params, Result, EncodedResult>
   ) =>
   (
-    impl: WebFunctionImpl<typeof spec>
-  ): WebFunction<Param, EncodedParam, Result, EncodedResult> => ({
-    spec,
-    impl,
-  });
+    impl: WebFunctionImpl<typeof def>
+  ): WebFunction<Name, Params, Result, EncodedResult> => ({ def, impl });
 
-const myStruct = Schema.TaggedStruct("MyFunction", { name: Schema.String });
-const test = mkWebFunction({
-  params: myStruct,
-  result: Schema.Either({ left: Schema.Number, right: Schema.String }),
-})(param => E.left(5));
+export const mkWebFunctionDef = <
+  Name extends string,
+  Params,
+  Result,
+  EncodedResult
+>(
+  name: Name,
+  params: Schema.Schema<Params>,
+  result: Schema.Schema<Result, EncodedResult>
+): WebFunctionDef<Name, Params, Result, EncodedResult> => ({
+  params: Schema.Struct({ _tag: Schema.Literal(name), params }),
+  result,
+});
 
-const tested = mkInvoke("")(test.spec)(myStruct.make({ name: "" }));
+// test
+const MyWebFunction = mkWebFunctionDef(
+  "MyWebFunction",
+  Schema.Struct({ hello: Schema.String }),
+  Schema.Either({ left: Schema.Number, right: Schema.String })
+);
 
-const parseParams =
-  (requestBody: string) =>
-  <P, P2, R, R2>(action: Action<P, P2, R, R2>) =>
-    pipe(
-      requestBody,
-      S.decodeOption(S.parseJson()),
-      O.flatMap(S.decodeUnknownOption(action.spec.params)),
-      O.map(params => [action, params] as const)
-    );
+const myWebFunction: WebFunctionImpl<typeof MyWebFunction> = ({ hello }) =>
+  E.right("not implemented");
 
-export const mkRequestHandler =
-  (webFunctions: WebFunction<unknown, unknown, unknown, unknown>[]) =>
-  (requestBodyJson: string) =>
-    pipe(
-      actions,
-      A.map(parseParams(requestBody)),
-      A.getSomes,
-      A.head,
-      O.map(([action, params]) => action.handler(params))
-    );
+const wf = mkWebFunction(MyWebFunction)(myWebFunction);
+
+const test = mkInvoke("")(MyWebFunction)({ hello: "john" });
