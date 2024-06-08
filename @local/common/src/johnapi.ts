0fa3e129c733690e1badc69bd70480664ad4e5d9
john <johnmichaelpost@gmail.com>
2024-06-08 00:03:22 -0500

    add type param to A.findFirst

--- @local/common/src/johnapi.ts
+++ @local/common/src/johnapi.ts
@@ -117,8 +117,9 @@ export const mkRequestHandler =
   (webFunctions: WebFunction<any, any, any, any>[]) => (jsonBody: string) =>
     pipe(
       webFunctions,
-      // TODO deal with stupid unknown error
-      A.findFirst(wf => Schema.is(Schema.parseJson(wf.def.params))(jsonBody)),
+      A.findFirst<WebFunction<any, any, any, any>>(wf =>
+        Schema.is(Schema.parseJson(wf.def.params))(jsonBody)
+      ),
       O.getOrThrowWith(() => "Request body did not match any web function"),
       executeWebFunction(jsonBody)
     );
