1dd72fedca118a74d6c0c380b67964821f20c459
john <johnmichaelpost@gmail.com>
2024-06-08 00:11:02 -0500

    change default lambda param

--- @local/api/src/index.ts
+++ @local/api/src/index.ts
@@ -8,7 +8,7 @@ const handleRequest = mkRequestHandler(webFunctions);
 export const handler = flow(
   (event: APIGatewayProxyEvent) => event.body,
   O.fromNullable,
-  O.getOrElse(() => "{}"),
+  O.getOrElse(() => ""),
   handleRequest,
   Ef.map(body => ({ statusCode: 200, body } as APIGatewayProxyResult)),
   Ef.runPromise
