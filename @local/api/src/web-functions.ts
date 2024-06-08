f9b835a5145f64b0588d9ea8b2244bef0ef0257c
john <johnmichaelpost@gmail.com>
2024-06-07 23:48:32 -0500

    put web function in array

--- @local/api/src/web-functions.ts
+++ @local/api/src/web-functions.ts
@@ -1,1 +1,4 @@
-export const webFunctions = [];
+import { mkWebFunction } from "@local/common/src/johnapi";
+import { GetTodos, getTodos } from "@local/common/src/web-functions";
+
+export const webFunctions = [mkWebFunction(GetTodos, getTodos)];
