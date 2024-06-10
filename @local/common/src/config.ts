29ef48d0e1850f8bbff6407ce144de344d2846ca
john <johnmichaelpost@gmail.com>
2024-06-09 19:56:50 -0500

    rename env var

--- @local/common/src/config.ts
+++ @local/common/src/config.ts
@@ -1,7 +1,7 @@
 import { Schema } from "./toolbox";
 
 export const Client = Schema.Struct({
-  CLIENT_WEB_FUNCTIONS_URL: Schema.String,
+  CLIENT_OPERATIONS_URL: Schema.String,
 });
 
 export const Server = Schema.Struct({
