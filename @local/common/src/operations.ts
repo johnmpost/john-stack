7542874e9ee51632a45b7e1101ce33ed438c7f81
john <johnmichaelpost@gmail.com>
2024-06-25 00:00:15 -0500

    try implement get and insert

--- @local/common/src/operations.ts
+++ @local/common/src/operations.ts
@@ -23,20 +23,46 @@ export const getTodos: OperationImpl<typeof GetTodos, Sql.client.Client> = () =>
 export const GetTodo = mkQueryDef(
   "GetTodo",
   Schema.Struct({ id: Schema.String }),
-  ({ id }) => ["todo", id],
+  ({ id }) => ["todos", id],
   Todo,
   NotFound,
 );
-export const getTodo: OperationImpl<typeof GetTodo, never> = () =>
-  Ef.succeed({
-    id: "8765",
-    title: "Single Todo",
-    description: "Just one. Gotten via GetTodo.",
-  });
+export const getTodo: OperationImpl<typeof GetTodo, Sql.client.Client> = ({
+  id,
+}) =>
+  pipe(
+    Sql.client.Client,
+    Ef.flatMap(sql =>
+      Sql.resolver.findById("GetTodoById", {
+        Id: Schema.String,
+        Result: Todo,
+        ResultId: _ => _.id,
+        execute: ids => sql`SELECT * FROM todos WHERE ${sql.in("id", ids)}`,
+      }),
+    ),
+    Ef.flatMap(x => x.execute(id)),
+    Ef.orDie,
+    Ef.flatten,
+    Ef.mapError(() => NotFound.make({})),
+  );
 
 export const CreateTodo = mkMutationDef("CreateTodo", Todo, Todo, Schema.Never);
-export const createTodo: OperationImpl<typeof CreateTodo, never> = ({
-  id,
-  title,
-  description,
-}) => Ef.succeed({ id, title, description });
+export const createTodo: OperationImpl<
+  typeof CreateTodo,
+  Sql.client.Client
+> = ({ id, title, description }) =>
+  pipe(
+    Sql.client.Client,
+    Ef.flatMap(sql =>
+      Sql.resolver.ordered("InsertTodo", {
+        Request: Todo,
+        Result: Todo,
+        execute: requests => sql`
+        INSERT INTO todos
+        ${sql.insert(requests)}
+        RETURNING todos.*`,
+      }),
+    ),
+    Ef.flatMap(x => x.execute({ id, title, description })),
+    Ef.orDie,
+  );
